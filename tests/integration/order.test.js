91fab28e5f186126a72d6ca58f03d9998cbd71b4
Kanesho1924 <avande764@gmail.com>
2024-06-10 10:50:40 +0200

    Fix Duplication Sonar

--- tests/integration/order.test.js
+++ tests/integration/order.test.js
@@ -113,7 +113,7 @@ describe('Orders API', () => {
         expect(response.text).toBe('Tous les champs date, id_produit, id_client, quantity et price sont obligatoires.');
     });
 
-    test('Erreur_400_CreateOrder_InvalidQuantity', async () => {
+    test('Erreur_400_CreateOrder_Type', async () => {
         const invalidOrder = {
             date: '2024-06-08',
             id_produit: 'prod123',
@@ -128,11 +128,11 @@ describe('Orders API', () => {
         expect(response.text).toBe('Le champ quantity doit être un nombre positif.');
     });
 
-    test('Erreur_400_CreateOrder_InvalidPrice', async () => {
+    test('Erreur_400_CreateOrder_Price', async () => {
         const invalidOrder = {
-            date: '2024-06-08',
-            id_produit: 'prod123',
-            id_client: 'client123',
+            date: '2024-07-10',
+            id_produit: 'laptop157',
+            id_client: 'a1b2c3d4e5',
             quantity: 10, 
             price: 'neuf euros quatre-vingt-dix neuf'
         };
@@ -199,22 +199,25 @@ describe('Orders API', () => {
 
     // Tests Erreurs 500
     describe('Erreur 500', () => {
+        beforeEach(() => {
+            db.collection = jest.fn(() => {
+                throw new Error();
+            });
+        });
+
         test('Erreur_500_GetOrders', async () => {
-            db.collection = function() { throw new Error() }
             const response = await request(app).get('/orders');
             expect(response.status).toBe(500);
             expect(response.text).toMatch(/Erreur lors de la récupération des commandes : /);
         });
 
         test('Erreur_500_GetOrderByID', async () => {
-            db.collection = function() { throw new Error() }
             const response = await request(app).get(`/orders/test`);
             expect(response.status).toBe(500);
             expect(response.text).toMatch(/Erreur lors de la récupération de la commande par ID : /);
         });
 
         test('Erreur_500_CreateOrder', async () => {
-            db.collection = function() { throw new Error() }
             const response = await createOrder({
                 date: '2024-06-08',
                 id_produit: 'prod123',
@@ -228,14 +231,12 @@ describe('Orders API', () => {
         });
 
         test('Erreur_500_UpdateOrder', async () => {
-            db.collection = function() { throw new Error() }
             const response = await updateOrderStatus('test', 'ValeurTest');
             expect(response.status).toBe(500);
             expect(response.text).toMatch(/Erreur lors de la mise à jour de la commande : /);
         });
 
         test('Erreur_500_DeleteOrder', async () => {
-            db.collection = function() { throw new Error() }
             const response = await deleteOrder('test');
             expect(response.status).toBe(500);
             expect(response.text).toMatch(/Erreur lors de la suppression de la commande : /);
